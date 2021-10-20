c6e411eb03a083dc71fb37bba62b545cabaf6005
Sam Smith <samlsmith424@gmail.com>
2021-10-19 18:34:40 -0700

    Add hashes to model-l.md

--- model-l.rb
+++ model-l.rb
@@ -9,3 +9,10 @@ z = 89.25
 am_hungry = true
 is_mean = false
 to_be_continued = true
+
+types_of_cheese = ['gruyere', 'cheddar', 'gouda']
+favorite_colors = ['green', 'blue', 'black']
+gym_exercises = ['squats', 'chest press', 'deadlift', 'rows']
+
+measurement = {"cm" => "inch", "kilometer" => "mile", "meter" => "yard"}
+veggies_at_home = {"zucchini" => 6, "potatoes" => 5, "carrots" => 7}
